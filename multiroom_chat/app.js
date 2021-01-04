f590032b44f54645ee9367ea8968068c972cec30
Luciano Clemente <lucianoaclemente@gmail.com>
2021-01-04 09:40:32 -0300

    Atualização

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -2,6 +2,17 @@
 var app = require('./config/server');
 
 // parametrizar a porta de escuta
-app.listen(3000, function() {
+var server = app.listen(3000, function() {
     console.log('Servidor online');
 })
+
+var io = require('socket.io').listen(server);
+
+// Criar a conexão por websocket
+io.on('connection', function(socket) {
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function() {
+        console.log('Usuário desconectou');
+    });
+});
