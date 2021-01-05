909f8d1a39239da672a02cf350ec56c3df3a7844
Luciano Clemente <lucianoaclemente@gmail.com>
2021-01-05 10:27:22 -0300

    Atualização

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,3 +1,36 @@
 module.exports.index = function(application, req, res) {
-    res.render('index');
+    res.render('index', { validacao : {} });
+}
+
+module.exports.autenticar = function(application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.render("index", {validacao : erros });
+        return;
+    }
+
+    var connection = application.config.dbConnection;
+    var usuariosDAO = new application.app.models.usuariosDAO(connection);
+
+    usuariosDAO.autenticar(dadosForm, function(result) {
+        req.session.autorizado = (result[0] !== undefined);
+
+        if (req.session.autorizado) {
+
+            req.session.usuario = result[0].usuario;
+            req.session.casa =  result[0].casa;
+
+            res.redirect('jogo');
+    
+        } else {
+            res.render("index", { validacao : {} })
+        }
+    });
+
 }
--- mmorpg_got/app/models/usuariosDAO.js
+++ mmorpg_got/app/models/usuariosDAO.js
@@ -12,6 +12,17 @@ usuariosDAO.prototype.inserirUsuario = function(usuario) {
     }); 
 }
 
+usuariosDAO.prototype.autenticar = function(usuario, callback) {
+    this._connection.open( function(erro, mongoClient) {
+        mongoClient.collection("usuarios", function(erro, collection) {
+            collection.find(usuario).toArray(function(erro, result) {
+                mongoClient.close();
+                callback(result);
+            });
+        });
+    }); 
+}
+
 module.exports = function() {
     return usuariosDAO;
 }
